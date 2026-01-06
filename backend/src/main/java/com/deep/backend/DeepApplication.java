5a2627b1dcf8469c271f29ac275564990ee0da31
YeonWoo <kobin4215@naver.com>
2026-01-06 13:08:50 +0900

    chore: move backend into /backend for monorepo

---
+++ backend/src/main/java/com/deep/backend/DeepApplication.java
@@ -0,0 +1,13 @@
+package com.deep.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepApplication.class, args);
+    }
+
+}
