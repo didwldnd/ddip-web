c4884ed044af6ad97e21b52e86156f1e6e48ee2f
kanghanseo <kanghanseo2015@gmail.com>
2026-01-07 15:06:11 +0900

    User 작성

--- backend/src/main/java/com/ddip/backend/entity/User.java
+++ backend/src/main/java/com/ddip/backend/entity/User.java
@@ -1,5 +1,9 @@
 package com.ddip.backend.entity;
 
+import com.ddip.backend.dto.oauth2.SocialUserRequestDto;
+import com.ddip.backend.dto.user.ProfileRequestDto;
+import com.ddip.backend.dto.user.UserRequestDto;
+import com.ddip.backend.dto.user.UserUpdateRequestDto;
 import com.ddip.backend.enums.BankType;
 import com.ddip.backend.enums.Role;
 import jakarta.persistence.*;
@@ -28,7 +32,7 @@ public class User extends BaseTimeEntity{
     private String password;
 
     @Column(name = "name", nullable = false)
-    private String name;
+    private String username;
 
     @Column(name = "nickname", nullable = false)
     private String nickname;
@@ -40,16 +44,62 @@ public class User extends BaseTimeEntity{
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    @Column(name = "bank_type", nullable = false)
+    @Column(name = "bank_type")
     @Enumerated(EnumType.STRING)
     private BankType bankType;
 
-    @Column(name = "account", nullable = false)
+    @Column(name = "account")
     private String account;
 
-    @Column(name = "account_holder", nullable = false)
+    @Column(name = "account_holder")
     private String accountHolder;
 
     @Column(name = "is_active", nullable = false)
-    private Boolean isActive = true;
+    private Boolean isActive;
+
+    public static User from(UserRequestDto dto) {
+        return User.builder()
+                .email(dto.getEmail())
+                .password(dto.getPassword())
+                .username(dto.getUsername())
+                .nickname(dto.getNickname())
+                .phoneNumber(dto.getPhoneNumber())
+                .role(Role.USER)
+                .bankType(dto.getBankType())
+                .account(dto.getAccount())
+                .accountHolder(dto.getAccountHolder())
+                .isActive(true)
+                .build();
+    }
+
+    public static User from(SocialUserRequestDto dto) {
+        return User.builder()
+                .email(dto.getEmail())
+                .username("TMP")
+                .nickname(dto.getNickname())
+                .phoneNumber("TMP")
+                .role(dto.getRole())
+                .isActive(false)
+                .build();
+    }
+
+    public void updateProfile(ProfileRequestDto dto) {
+        this.username = dto.getUsername();
+        this.phoneNumber = dto.getPhoneNumber();
+    }
+
+    public void update(UserUpdateRequestDto updateRequest) {
+        this.email = updateRequest.getEmail();
+        this.password = updateRequest.getPassword();
+        this.username = updateRequest.getUsername();
+        this.nickname = updateRequest.getNickname();
+        this.phoneNumber = updateRequest.getPhoneNumber();
+        this.account = updateRequest.getAccount();
+        this.accountHolder = updateRequest.getAccountHolder();
+        this.bankType = updateRequest.getBankType();
+    }
+
+    public void updatePassword(String password) {
+        this.password = password;
+    }
 }
